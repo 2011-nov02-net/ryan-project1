26872e412eed454475171a8217f8cef64a826c81
Ryan Towner <townerr@yahoo.com>
2020-12-03 12:04:12 -0600

    Updated context

--- Project1/Project1.DataAccess/Entities/project1Context.cs
+++ Project1/Project1.DataAccess/Entities/project1Context.cs
@@ -29,7 +29,7 @@ namespace Project1.DataAccess.Entities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:towner-training.database.windows.net,1433;Initial Catalog=project1;Persist Security Info=False;User ID=rtowner;Password=MazdaSpeed3;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("");
             }
         }
 
